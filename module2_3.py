4e30bb8a1f9cb4c886a7f5cd6a500d98a4ee1d2a
Lufanin <orion.pax37@gmail.com>
2024-06-16 01:01:24 +0300

    Add files via upload

---
+++ module2_3.py
@@ -0,0 +1,11 @@
+b = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while True:
+    if my_list[b] > 0:
+        print(my_list[b])
+        b += 1
+    elif my_list[b] == 0:
+        b += 1
+        continue
+    elif my_list[b] < 0:
+        break
